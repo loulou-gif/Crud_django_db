fac398503054f4c4eb1a855dd1961b67f55a5656
loulou-gif <konankanjulius10@gmail.com>
2025-03-18 20:30:09 +0000

    commit màj department

--- registre/settings.py
+++ registre/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-_uy%@p)0sb*f=3l-bv)4q+ls_e*wpvnpf&4bj3fh=0u-70ls2x'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
@@ -74,7 +74,7 @@ WSGI_APPLICATION = 'registre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+'''DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'annexe',
@@ -82,6 +82,13 @@ DATABASES = {
 	'PASSWORD': 'K4dk272001@',
 	'HOST':	'localhost',
 	'PORT':	'5432',
+    }
+}'''
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',  # Moteur de base de données
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),  # Emplacement du fichier SQLite
     }
 }
 
